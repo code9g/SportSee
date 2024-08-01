8b4400e2a183c8ec863fc4396d8acd0e5ff64811
Pierre HENRY <pierre.henry@code99.fr>
2024-08-01 16:28:06 +0200

    JSDoc useFetch

--- front/src/hooks/useFetch.js
+++ front/src/hooks/useFetch.js
@@ -1,5 +1,17 @@
 import { useEffect, useState } from "react";
 
+/**
+ * Hook de gestion du fetch des données
+ *
+ * Ce hook permet de gérer le chargement, les erreurs et les données à récupèrer
+ *
+ * @param {Number} id - L'identifiant de l'utilisateur
+ * @param {Function} api - L'api a utiliser pour récupèrer les données
+ * @param {String} title - Titre informatif des données pour l'affichage des données et des erreurs dans la console
+ * @param {Object|Array<Object>|Null} defaultData - Données par défaut
+ *
+ * @returns {Object} - Un objet contenant isLoading, error et data
+ */
 function useFetch(id, api, title, defaultData = []) {
   const [data, setData] = useState(defaultData);
   const [isLoading, setIsLoading] = useState(false);
