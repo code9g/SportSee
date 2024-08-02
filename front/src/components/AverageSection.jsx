c7b4d8fe3c2ac9d0456dac13f8125d5bc6da8151
Pierre HENRY <pierre.henry@code99.fr>
2024-08-02 11:38:12 +0200

    rafactoring Average

--- front/src/components/AverageSection.jsx
+++ front/src/components/AverageSection.jsx
@@ -1,50 +1,18 @@
 import PropTypes from "prop-types";
-import useFetch from "../hooks/useFetch";
-import fetchUserAverageSessionsApi from "../services/fetchUserAverageSessionsApi";
-import { dayOfWeek } from "../utils/consts";
-import Error from "./Error";
-import Loading from "./Loading";
-import NoData from "./NoData";
-import AverageChart from "./charts/AverageChart";
+import Average from "./Average";
 
 /**
- * Composant pour afficher la durée moyenne des sessions de l'utilisateur sous forme de graphique linéaire.
- *
- * Ce composant récupère les données de session pour un utilisateur spécifique et les affiche en utilisant
- * un graphique linéaire. La ligne représente la durée des sessions, avec des détails supplémentaires affichés
- * dans un tooltip personnalisé lors du survol de la ligne.
+ * Composant pour afficher la durée moyenne des sessions de l'utilisateur sous forme de graphique linéaire
+ * dans une section.
  *
  * @param {Object} props Les propriétés du composant.
  * @param {number} props.user L'utilisateur pour lequel les données de session sont récupérées.
  * @returns {JSX.Element} Un élément JSX contenant le graphique linéaire et le titre associé.
  */
 function AverageSection({ user }) {
-  const {
-    isLoading,
-    error,
-    data: average,
-  } = useFetch(user.id, fetchUserAverageSessionsApi, "average-sessions", []);
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
-  if (error) {
-    return <Error message={error} />;
-  }
-
-  if (!average || average.length === 0) {
-    return <NoData />;
-  }
-
-  const data = average.map((item) => ({
-    day: dayOfWeek[item.day],
-    sessionLength: item.sessionLength,
-  }));
-
   return (
     <section className="average">
-      <AverageChart data={data} />
+      <Average user={user} />
     </section>
   );
 }
