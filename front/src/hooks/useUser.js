a4c19e42a9dd53e18ec94ae147e68a6cfdc45a93
Pierre HENRY <pierre.henry@code99.fr>
2024-08-02 21:28:44 +0200

    Add jsdoc useUser

--- front/src/hooks/useUser.js
+++ front/src/hooks/useUser.js
@@ -1,6 +1,11 @@
 import { useContext } from "react";
 import UserContext from "../contexts/UserContext";
 
+/**
+ * Hook de gestion du contexte UserContext
+ *
+ * @returns {React.Context}
+ */
 const useUser = () => useContext(UserContext);
 
 export default useUser;
