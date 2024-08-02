658972f87154e85c119b432de130a9951d09ec1a
Pierre HENRY <pierre.henry@code99.fr>
2024-08-02 21:32:47 +0200

    jsdoc update

--- front/src/providers/UserProvider.jsx
+++ front/src/providers/UserProvider.jsx
@@ -1,6 +1,16 @@
 import PropTypes from "prop-types";
 import UserContext from "../contexts/UserContext";
 
+/**
+ * Composant de mise en place d'un provider du contexte UserContext
+ *
+ * Permet de "partager" les données d'un utilisateur (user) à tous ses enfants (children)
+ *
+ * @param {{ user: Object; children: any; }} props
+ * @param {Object} props.user
+ * @param {*} props.children
+ * @returns {JSX.Element} Retourne un JSX élément du provider du contexte UserContext avec ses enfants
+ */
 function UserProvider({ user, children }) {
   const value = { user };
 
