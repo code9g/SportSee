76c519a96fd8b1e731d033bce4b9bde2af7e91bf
Pierre HENRY <pierre.henry@code99.fr>
2024-08-02 21:35:33 +0200

    jsdoc

--- front/src/contexts/UserContext.jsx
+++ front/src/contexts/UserContext.jsx
@@ -1,5 +1,10 @@
 import { createContext } from "react";
 
+/**
+ * Définition du contexte UserContext
+ *
+ * @type {React.Context}
+ */
 const UserContext = createContext(null);
 
 export default UserContext;
