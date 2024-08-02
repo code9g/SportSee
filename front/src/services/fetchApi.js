f80a1285832c9cfb1081a0709f13224f66d4ec6c
Pierre HENRY <pierre.henry@code99.fr>
2024-08-02 21:36:14 +0200

    update param

--- front/src/services/fetchApi.js
+++ front/src/services/fetchApi.js
@@ -7,12 +7,12 @@ import { URL_API } from "../utils/consts";
  * de l'utilisateur (userId) et du chemin (path) pour interroger une API REST
  *
  * @async
- * @param {number} userId Identifiant de l'utilisateur
+ * @param {number} id Identifiant de l'utilisateur
  * @param {string} path Endpoints de l'API REST
  * @returns {Promesse} Retourne une promesse
  */
-const fetchApi = async (userId, path, ...args) =>
-  fetch(`${URL_API}/user/${userId}${path}`, ...args)
+const fetchApi = async (id, path, ...args) =>
+  fetch(`${URL_API}/user/${id}${path}`, ...args)
     .then((response) => response.json())
     .then((json) => json?.data);
 
